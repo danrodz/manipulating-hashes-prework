0a7f233b8bcaaa7615093f258a2bd3ab66647ce1
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-26 02:39:15 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
   contacts["Freddy Mercury"].each do |attribute, value|
     if attribute == :favorite_icecream_flavors
       value.each do |flavor|
-        value.delete_if
+        value.delete_if flavor == "strawberry"
       end
     end
   end
